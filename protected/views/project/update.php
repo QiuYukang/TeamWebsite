194622fb620bc65411e031eaa5d0a8335509910e
mirraico <feixuan2013@gmail.com>
2017-02-09 18:12:30 +0800

    ADD: project -upload

--- protected/views/project/update.php
+++ protected/views/project/update.php
@@ -3,6 +3,7 @@
 /* @var $model Project */
 
 
+$this->pageTitle=Yii::app()->name . ' - 编辑科研项目';
 //面包屑
 $this->breadcrumbs=array(
     '科研'=>array('project/index'),
@@ -14,14 +15,29 @@ $this->breadcrumbs=array(
 
 ?>
 
-<div style="position:relative">
-    <img src="images/lang1.jpg" alt="" />
-    <div style="position:absolute;z-indent:2;left:0;top:0;">
-        <br>
-        <h2>编辑科研项目</h2>
-        <h4><?php echo substr($model->name, 0, 30).'...'; ?></h4>
+<div class="cam-page-header">
+    <div class="cam-wrap clearfix cam-local-navigation">
+        <ul class="cam-unstyled-list cam-current">
+            <li><a href="index.php?r=site/introduction">团队介绍</a></li>
+            <li><a href="index.php?r=site/direction">研究方向</a></li>
+            <li class="cam-current-page"><a href="#" class="active-trail">科研项目</a></li>
+            <li><a href="#">科研成果</a></li>
+        </ul>
+    </div>
+    <div class="cam-wrap clearfix cam-page-sub-title cam-recessed-sub-title">
+        <div class="cam-column">
+            <div class="cam-content-container">
+                <h1 class="cam-sub-title">
+                    添加科研项目 Add project
+                </h1>
+            </div>
+        </div>
     </div>
 </div>
-
-
-<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+<div class="cam-content cam-recessed-content">
+    <div class="cam-wrap clearfix">
+        <div class="index-content">
+            <?php $this->renderPartial('_form', array('model'=>$model)); ?>
+        </div>
+    </div>
+</div>
