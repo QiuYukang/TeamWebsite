7d9db4c1e28dc848a41bb78e9c989102c1cf5b0b
mirraico <feixuan2013@gmail.com>
2017-02-10 12:12:27 +0800

    ADD: paper - view

--- protected/views/paper/view.php
+++ protected/views/paper/view.php
@@ -12,6 +12,7 @@
 //    array('label'=>'导入论文', 'url'=>array('upload')),
 //);
 
+$this->pageTitle=Yii::app()->name . ' - 论文详情';
 //面包屑
 $this->breadcrumbs=array(
     '学术成果'=>array('paper/index'),
@@ -21,99 +22,114 @@ $this->breadcrumbs=array(
 );
 ?>
 
-<div style="position:relative">
-    <img src="images/lang1.jpg" alt="" />
-    <div style="position:absolute;z-indent:2;left:0;top:0;">
-        <br>
-        <h2>论文详细</h2>
-        <h4><?php echo substr($model->info, 0, 30).'...'; ?></h4>
+<div class="cam-page-header">
+    <div class="cam-wrap clearfix cam-local-navigation">
+        <ul class="cam-unstyled-list cam-current">
+            <li class="cam-current-page"><a href="index.php?r=paper/admin" class="active-trail">论文</a></li>
+            <li><a href="index.php?r=patent/admin">专利</a></li>
+            <li><a href="index.php?r=publication/admin">专著</a></li>
+            <li><a href="index.php?r=software/admin">软件著作权</a></li>
+        </ul>
+    </div>
+    <div class="cam-wrap clearfix cam-page-sub-title cam-recessed-sub-title">
+        <div class="cam-column">
+            <div class="cam-content-container">
+                <h1 class="cam-sub-title">
+                    论文详情 Paper details
+                </h1>
+            </div>
+        </div>
     </div>
 </div>
 
-<br>
-<?php
-    echo '<div class="errorMessage">'.$model->getIncompleteInfo().'</div>';
-?>
-<br>
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-        'info',
-        array(
-            'label'=>'作者',
-            'type'=>'raw',
-            'value'=>$model->getAuthorsWithLink() != '' ? $model->getAuthorsWithLink() : null,
-        ),
-        array(
-            'label'=>'状态',
-            'type'=>'raw',
-            'value'=>$model->getStatusString(true) != '' ? $model->getStatusString(true) : null,
-        ),
-        array(
-            'label'=>'类别',
-            'type'=>'raw',
-            'value'=>$model->getCategoryString() != '' ? $model->getCategoryString() : null,
-        ),
-        'pass_date',
-        'pub_date',
-        'index_date',
-        'sci_number',
-        'ei_number',
-        'istp_number',
-        array(
-            'label'=>'论文文件',
-            'type'=>'raw',
-            'value'=>
-                isset($model->file_name) ?
-                    ( isset($model->file_content) ? CHtml::link($model->file_name,array('download','id'=>$model->primaryKey)) : CHtml::encode($model->file_name.'  (未上传文件)') )
-                 : null,
-        ),
-        array(
-            'label'=>'支助项目',
-            'type'=>'raw',
-            'value'=>$model->getProjectsWithLink(Paper::PROJECT_FUND) != '' ?
-                $model->getProjectsWithLink(Paper::PROJECT_FUND) :
-                null,
-        ),
-        array(
-            'label'=>'报账项目',
-            'type'=>'raw',
-            'value'=>$model->getProjectsWithLink(Paper::PROJECT_REIM) != '' ?
-                $model->getProjectsWithLink(Paper::PROJECT_REIM) :
-                null,
-        ),
-        array(
-            'label'=>'成果项目',
-            'type'=>'raw',
-            'value'=>$model->getProjectsWithLink(Paper::PROJECT_ACHIEVEMENT) != '' ?
-                $model->getProjectsWithLink(Paper::PROJECT_ACHIEVEMENT) :
-                null,
-        ),
-        array(
-            'label'=>'维护人员',
-            'type'=>'raw',
-            'value'=>
-                isset($model->maintainer) ?
-                    CHtml::link(CHtml::encode($model->maintainer->name), array('people/view', 'id'=>$model->maintainer->id)) :
-                    null,
-        ),
-        'last_update_date',
-	),
-)); ?>
-<hr/>
+<div class="cam-content cam-recessed-content">
+    <div class="cam-wrap clearfix">
+        <div class="index-content">
+            <?php
+                echo '<div class="errorMessage">'.$model->getIncompleteInfo().'</div>';
+            ?>
+            <?php $this->widget('zii.widgets.CDetailView', array(
+                'data'=>$model,
+                'attributes'=>array(
+                    'info',
+                    array(
+                        'label'=>'作者',
+                        'type'=>'raw',
+                        'value'=>$model->getAuthorsWithLink() != '' ? $model->getAuthorsWithLink() : null,
+                    ),
+                    array(
+                        'label'=>'状态',
+                        'type'=>'raw',
+                        'value'=>$model->getStatusString(true) != '' ? $model->getStatusString(true) : null,
+                    ),
+                    array(
+                        'label'=>'类别',
+                        'type'=>'raw',
+                        'value'=>$model->getCategoryString() != '' ? $model->getCategoryString() : null,
+                    ),
+                    'pass_date',
+                    'pub_date',
+                    'index_date',
+                    'sci_number',
+                    'ei_number',
+                    'istp_number',
+                    array(
+                        'label'=>'论文文件',
+                        'type'=>'raw',
+                        'value'=>
+                            isset($model->file_name) ?
+                                ( isset($model->file_content) ? CHtml::link($model->file_name,array('download','id'=>$model->primaryKey)) : CHtml::encode($model->file_name.'  (未上传文件)') )
+                             : null,
+                    ),
+                    array(
+                        'label'=>'支助项目',
+                        'type'=>'raw',
+                        'value'=>$model->getProjectsWithLink(Paper::PROJECT_FUND) != '' ?
+                            $model->getProjectsWithLink(Paper::PROJECT_FUND) :
+                            null,
+                    ),
+                    array(
+                        'label'=>'报账项目',
+                        'type'=>'raw',
+                        'value'=>$model->getProjectsWithLink(Paper::PROJECT_REIM) != '' ?
+                            $model->getProjectsWithLink(Paper::PROJECT_REIM) :
+                            null,
+                    ),
+                    array(
+                        'label'=>'成果项目',
+                        'type'=>'raw',
+                        'value'=>$model->getProjectsWithLink(Paper::PROJECT_ACHIEVEMENT) != '' ?
+                            $model->getProjectsWithLink(Paper::PROJECT_ACHIEVEMENT) :
+                            null,
+                    ),
+                    array(
+                        'label'=>'维护人员',
+                        'type'=>'raw',
+                        'value'=>
+                            isset($model->maintainer) ?
+                                CHtml::link(CHtml::encode($model->maintainer->name), array('people/view', 'id'=>$model->maintainer->id)) :
+                                null,
+                    ),
+                    'last_update_date',
+                ),
+            )); ?>
+            <br/>
 
-<?php
-//删除必须使用POST提交，这里写一个表单
-//admin manager用户才能编辑、删除
-$user = Yii::app()->user;
-?>
-<form name="operate" action="<?php echo Yii::app()->controller->createUrl("delete",array("id"=>$model->id)); ?>" method="post" onsubmit="return firm()">
-    <?php if(isset($user->is_admin) && $user->is_admin || isset($user->is_manager) && $user->is_manager) { ?>
-    <input type="button" value="编辑" class="btn btn-default" onclick="location='<?php echo Yii::app()->controller->createUrl("update",array("id"=>$model->id)); ?>'"/>
-    <input type="submit" class="btn btn-default" id="drop"  value="删除"/>
-    <?php } ?>
-    <input type="button" class="btn btn-default" value="返回" onclick="location='<?php echo Yii::app()->controller->createUrl("admin"); ?>'"/>
-</form>
+            <?php
+            //删除必须使用POST提交，这里写一个表单
+            //admin manager用户才能编辑、删除
+            $user = Yii::app()->user;
+            ?>
+            <form name="operate" action="<?php echo Yii::app()->controller->createUrl("delete",array("id"=>$model->id)); ?>" method="post" onsubmit="return firm()">
+                <?php if(isset($user->is_admin) && $user->is_admin || isset($user->is_manager) && $user->is_manager) { ?>
+                <input type="button" value="编辑" class="btn btn-default" onclick="location='<?php echo Yii::app()->controller->createUrl("update",array("id"=>$model->id)); ?>'"/>
+                <input type="submit" class="btn btn-default" id="drop"  value="删除"/>
+                <?php } ?>
+                <input type="button" class="btn btn-default" value="返回" onclick="location='<?php echo Yii::app()->controller->createUrl("admin"); ?>'"/>
+            </form>
+        </div>
+    </div>
+</div>
 
 <script>
     function firm() {
